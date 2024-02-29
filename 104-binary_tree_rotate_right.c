48bd5cbb28b0c279dae06d3aaa94b145087cae9b
cox101 <cox2659@gmail.com>
2024-02-29 13:31:20 -0800

    Initial commit

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -5,27 +5,27 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
     if (tree == NULL || tree->left == NULL)
         return tree;
 
-    binary_tree_t *new_root = tree->left;
-    binary_tree_t *temp = new_root->right;
+    binary_tree_t *root = tree->left;
+    binary_tree_t *temp = root->right;
 
     /* Perform rotation */
-    new_root->right = tree;
-    tree->parent = new_root;
+    root->right = tree;
+    tree->parent = root;
     tree->left = temp;
 
     if (temp != NULL)
         temp->parent = tree;
 
-    new_root->parent = tree->parent;
+    root->parent = tree->parent;
 
     if (tree->parent != NULL)
     {
         if (tree->parent->left == tree)
-            tree->parent->left = new_root;
+            tree->parent->left = root;
         else
-            tree->parent->right = new_root;
+            tree->parent->right = root;
     }
 
-    return new_root;
+    return root;
 }
 
