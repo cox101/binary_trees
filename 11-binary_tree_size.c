ba826c1b516db258b502f469cf36918c5d39c2a9
cox101 <cox2659@gmail.com>
2024-02-29 04:18:11 -0800

    Initial commit '

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -11,11 +11,5 @@ size_t binary_tree_size(const binary_tree_t *tree)
     if (tree == NULL)
         return 0;
 
-    /* Recursively calculate the size of the left and right subtrees */
-    size_t left_size = binary_tree_size(tree->left);
-    size_t right_size = binary_tree_size(tree->right);
-
-    /* Return the size of the current node plus the sizes of its subtrees */
-    return (1 + left_size + right_size);
+    return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
-
