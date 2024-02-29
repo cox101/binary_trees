f91b71268ef6dbaeb6a8dc59adfcccad77c722c2
cox101 <cox2659@gmail.com>
2024-02-29 04:53:35 -0800

    initial commit

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_leaves - counts the leaves in a binary tree
- * @tree: pointer to the root node of the tree to count leaves
+ * @tree: pointer to the root node of the tree
  *
  * Return: number of leaves in the binary tree, or 0 if tree is NULL
  */
