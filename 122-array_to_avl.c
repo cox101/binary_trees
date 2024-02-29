27577d4b1fba0b5c6527bbea955877448bbbfc39
cox101 <cox2659@gmail.com>
2024-02-29 15:02:00 -0800

    Initial commit

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -12,12 +12,12 @@ avl_t *array_to_avl(int *array, size_t size)
     avl_t *root = NULL;
     size_t i;
 
-    if (array == NULL || size == 0)
+    if (array == NULL)
         return (NULL);
 
     for (i = 0; i < size; i++)
     {
-        if (avl_insert(&root, array[i]) == NULL)
+        if (binary_tree_insert(&root, array[i]) == NULL)
         {
             /* Insertion failed, free the tree and return NULL */
             binary_tree_delete(root);
